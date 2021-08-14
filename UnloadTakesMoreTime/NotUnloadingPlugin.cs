dd9565be6833bf714daef2f2e79468f43db167d8
František Boháček <fandabohacek@gmail.com>
2021-08-14 13:06:56 +0200

    docs: update comments with information for release version

--- UnloadTakesMoreTime/NotUnloadingPlugin.cs
+++ UnloadTakesMoreTime/NotUnloadingPlugin.cs
@@ -28,8 +28,12 @@ namespace DoesntUnload
             // works okay with char, string, int, object (seems like that it works okay with all built-in types, but I did not test each one of them)
             // unload takes more time for custom classes/records, structs being iterated over
             // okay means that it unloads correctly on second iteration of GC.Collect (waiting for 5000 + 2000 ms)
+            
+            // For debug
             // unload takes more time means it unloads correctly on 49th iteration or later. (5000 + 49000 ms at least for me, may be different for you)
             // Some runs took 49 iterations, some even 420 iterations
+            
+            // In release it took 21 iterations
             foreach (var element in elements)
             {
                 Console.WriteLine(element);
